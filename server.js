bbecbda582036c6e7983cca05a4d465e7a6244e9
bjupreti <bjupreti123@gmail.com>
2020-05-17 21:31:29 +0545

    Feedback Service and Speaker Service added

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const path = require('path');
 const routes = require('./routes');
 
+const FeedbackService = require('./services/FeedbackService');
+const SpeakerService = require('./services/SpeakerService');
+
+const feedbackService = new FeedbackService('./data/feedback.json');
+const speakerService = new SpeakerService('./data/speakers.json');
+
 const app = express();
 
 const port = 3000;
@@ -14,7 +20,13 @@ app.set('views', path.join(__dirname, './views'));
 // using satic middleware
 app.use(express.static(path.join(__dirname, './static')));
 
-app.use('/', routes());
+app.use(
+  '/',
+  routes({
+    feedbackService,
+    speakerService,
+  })
+);
 
 app.listen(port, () => {
   console.log(`Node server running on ${port}`);

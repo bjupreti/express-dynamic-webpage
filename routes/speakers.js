d5d8ad30ced52510683f3ed3a8112f17e3ebb6b0
bjupreti <bjupreti123@gmail.com>
2020-05-19 00:12:18 +0545

    Speaker and speaker list made dynamic

--- routes/speakers.js
+++ routes/speakers.js
@@ -6,12 +6,20 @@ module.exports = (params) => {
   const { speakerService } = params;
 
   router.get('/', async (request, response) => {
+    const artwork = await speakerService.getAllArtwork();
     const speakers = await speakerService.getList();
-    return response.json(speakers);
+    response.render('layout', { pageTitle: 'Speakers', template: 'speakers', speakers, artwork });
   });
 
-  router.get('/:shortname', (request, response) => {
-    return response.send(`Details page of ${request.params.shortname}`);
+  router.get('/:shortname', async (request, response) => {
+    const artwork = await speakerService.getArtworkForSpeaker(request.params.shortname);
+    const speaker = await speakerService.getSpeaker(request.params.shortname);
+    response.render('layout', {
+      pageTitle: speaker.name,
+      template: 'speaker-details',
+      speaker,
+      artwork,
+    });
   });
 
   return router;
